7842a9dd5cd9e62a2b57318ba716ec0df58279c0
Muhammed <muhammed@sendme.ng>
2023-07-30 12:37:22 +0100

    complete assignment

--- bash/week3/percentage_value.sh
+++ bash/week3/percentage_value.sh
@@ -7,3 +7,12 @@ percent=$2
 #######################################################
 ############# write your solution below   ############# 
 #######################################################
+
+# Check if value and percentage are provided
+if [ $# -ne 2 ]; then
+    echo "bash $0 <value> <percent>"
+    exit 1
+fi
+
+result=$((value * percent / 100))
+echo $result
--- bash/week3/task2.sh
+++ bash/week3/task2.sh
@@ -3,3 +3,12 @@
 ####################################################
 ############# write your solution below   ############# 
 #######################################################
+
+# Check if file exists
+if [ $# -ne 1 ]; then
+    echo "File data.txt not passed"
+    exit 1
+fi
+
+# print the details of all male gender in the file
+grep "Male" data.txt
