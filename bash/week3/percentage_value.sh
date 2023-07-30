2bc365b9c0d759e23a31d34a3c72849e0ea64512
Muhammed <muhammed@sendme.ng>
2023-07-30 12:05:07 +0100

    feat: week3 assignmemt

---
+++ bash/week3/percentage_value.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# calculate the percentage of the value below. Your anser should be in two decimal places
+value=$1
+percent=$2
+
+#######################################################
+############# write your solution below   ############# 
+#######################################################
